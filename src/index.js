74c583743f180ff9bb138b2c2cee55f7fbbca7d1
Alvin Acosta <alvin.acosta@movon.com.ph>
2022-10-07 13:39:34 +0800

    add disable dev tools

--- src/index.js
+++ src/index.js
@@ -8,6 +8,10 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Provider } from "react-redux";
 import { store } from "./app/store";
 
+import { disableReactDevTools } from "@fvilers/disable-react-devtools";
+
+if(process.env.NODE_ENV === "production") disableReactDevTools()
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
